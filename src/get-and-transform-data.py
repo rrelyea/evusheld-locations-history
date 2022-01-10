02b72eb879be49170c5df9ebe3e13c89bed34f1f
Rob Relyea <rob@relyeas.net>
2022-01-10 15:43:08 -0800

    move some CSVs into data folder

--- src/get-and-transform-data.py
+++ src/get-and-transform-data.py
@@ -17,10 +17,10 @@ del providerData['last_date_delivered']
 del providerData['total_courses']
 del providerData['courses_available']
 del providerData['courses_available_date']
-providerData.to_csv("provider-data.csv", index=False)
+providerData.to_csv("data/provider-data.csv", index=False)
 
-hhs_ids = pd.read_csv("HHS_IDs.csv")
+hhs_ids = pd.read_csv("data/HHS_IDs.csv")
 
 geocodes = pd.DataFrame(providerData.geocoded_address.unique(), None, ["geocoded_address"])
 geo_hhsid = geocodes.merge(hhs_ids, 'left', left_on='geocoded_address', right_on='geocoded_hospital_address')
-geo_hhsid.to_csv("geo-hhsids.csv", index=False)
+geo_hhsid.to_csv("data/geo-hhsids.csv", index=False)
