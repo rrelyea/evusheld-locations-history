ecd7b411b84573f387431d0c9ddb0b6edf658b53
Rob Relyea <rob@relyeas.net>
2022-02-10 09:56:27 -0800

    start scraping paxlovid data also

--- src/get-and-transform-data.py
+++ src/get-and-transform-data.py
@@ -1,30 +1,18 @@
 import pandas as pd
+
+def sortDataFile(fileName):
+  dataFile = pd.read_csv(fileName)
   
-evusheldData = pd.read_csv("evusheld-data.csv")
+  # ensure standard casing, so that sorting works better.
+  dataFile['city'] = dataFile['city'].str.title()
+  dataFile['county'] = dataFile['county'].str.title()
 
-# ensure standard casing, so that sorting works better.
-evusheldData['city'] = evusheldData['city'].str.title()
-evusheldData['county'] = evusheldData['county'].str.title()
+  dataFile.sort_values(["state_code", "county", "city", "provider_name"], 
+                      axis=0,
+                      ascending=[True, True, True, True], 
+                      inplace=True)
 
-evusheldData.sort_values(["state_code", "county", "city", "provider_name"], 
-                    axis=0,
-                    ascending=[True, True, True, True], 
-                    inplace=True)
-  
-evusheldData.to_csv("evusheld-data.csv", index=False)
+  dataFile.to_csv(fileName, index=False)
 
-providerData = evusheldData
-del providerData['national_drug_code']
-del providerData['order_label']
-del providerData['last_order_date']
-del providerData['last_date_delivered']
-del providerData['total_courses']
-del providerData['courses_available']
-del providerData['courses_available_date']
-providerData.to_csv("data/provider-data.csv", index=False)
-
-hhs_ids = pd.read_csv("data/HHS_IDs.csv")
-
-geocodes = pd.DataFrame(providerData.geocoded_address.unique(), None, ["geocoded_address"])
-geo_hhsid = geocodes.merge(hhs_ids, 'left', left_on='geocoded_address', right_on='geocoded_hospital_address')
-geo_hhsid.to_csv("data/geo-hhsids.csv", index=False)
+sortDataFile("evusheld-data.csv")
+sortDataFile("paxlovid-data.csv")
