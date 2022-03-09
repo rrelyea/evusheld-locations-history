c6c4aa7bc341dce996d07e00370c0324aff4ceff
Rob Relyea <rob@relyeas.net>
2022-03-09 08:52:06 -0800

    automation updating of dose data history per zip code. Fixes #12

--- src/process-therapeutics.py
+++ src/process-therapeutics.py
@@ -2,20 +2,21 @@ from fileinput import filename
 import json
 from io import StringIO
 import os
+import sys
 from os.path import exists
 import requests
 from urllib.parse import urlparse
 import csv
 
-debuggingLocal = False
-localBasePath = "../" if debuggingLocal else ""
+# locally, when I test, I need to pass "../../" as argv[1]
+localBasePath = sys.argv[1] if len(sys.argv) > 1 else ""
 
-with open(localBasePath + "../data/therapeutics-last-processed.txt", "r") as lastProcessed_file:
+with open(localBasePath + "data/therapeutics-last-processed.txt", "r") as lastProcessed_file:
   lastProcessedDate = lastProcessed_file.readline()
   print("last processed: " + lastProcessedDate)
 
 newLastProcessDate = None
-with open(localBasePath + "../data/therapeutics-archive.json", "r") as read_file:
+with open(localBasePath + "data/therapeutics-archive.json", "r") as read_file:
     publishings = json.load(read_file)
     urls = []
 
@@ -28,47 +29,51 @@ with open(localBasePath + "../data/therapeutics-archive.json", "r") as read_file
 # download all new files
 for url in sorted(urls):
   filename = os.path.basename(urlparse(url).path)
-  localFile = 'archive/'+filename
+  localFile = localBasePath + 'data/therapeutics/' + filename
   if not exists(localFile):
     r = requests.get(url, allow_redirects=True)
     open(localFile, 'wb').write(r.content)
 
-zipSet = set()
-url = sorted(urls)[len(urls) - 1]
-filename = os.path.basename(urlparse(url).path)
-localFile = 'archive/'+filename
-with open(localFile, 'r',encoding='utf8') as data:
-  reader = csv.reader(data)
-  for columns in reader:
-    zip = columns[6]
-    if zip != "Zip" and 'Evusheld' == columns[8]:
-      zipSet.add(zip[0:5])
+if len(urls) > 0:
+    # calculate all zip codes, by looking at latest data file
+    # note - if a provider was listed earlier, but isn't in the latest data file, their zip code might not be processed
+    #      - tracking this problem here: https://github.com/rrelyea/evusheld-locations-history/issues/11
+    zipSet = set()
+    url = sorted(urls)[len(urls) - 1]
+    filename = os.path.basename(urlparse(url).path)
+    localFile = localBasePath + 'data/therapeutics/'+filename
+    with open(localFile, 'r',encoding='utf8') as data:
+      reader = csv.reader(data)
+      for columns in reader:
+        zip = columns[6]
+        if zip != "Zip" and 'Evusheld' == columns[8]:
+          zipSet.add(zip[0:5])
 
-print(str(len(zipSet)) + ' zipcodes:')
-for zipCode in sorted(zipSet):
-  print(zipCode, end=', ', flush=True)
+    print(str(len(zipSet)) + ' zipcodes:')
+    for zipCode in sorted(zipSet):
+      print(zipCode, end=', ', flush=True)
 
-  with open(localBasePath + '../data/dose-details/'+str(zipCode)+'.csv', "a") as f:
-    for url in sorted(urls):
-      filename = os.path.basename(urlparse(url).path)
-      localFile = 'archive/'+filename
-      timeStamp = filename.replace('rxn6-qnx8_','').replace('.csv','')
-      with open(localFile, 'r',encoding='utf8') as data:
-        reader = csv.reader(data)
-        for columns in reader:
-          zip = columns[6]
-          provider = columns[0]
-          if "," in provider:
-            provider = '"' + provider + '"'
-          if zip[0:5] == zipCode and 'Evusheld' == columns[8]:
-            f.write(timeStamp + ',' + zip + ',' + provider)
-            for i in range(9, 14):
-              if i < len(columns):
-                f.write(',' + columns[i])
-              else:
-                f.write(',')
-            f.write('\n')
-    f.close()
+      with open(localBasePath + 'data/dose-details/'+str(zipCode)+'.csv', "a") as f:
+        for url in sorted(urls):
+          filename = os.path.basename(urlparse(url).path)
+          localFile = 'archive/'+filename
+          timeStamp = filename.replace('rxn6-qnx8_','').replace('.csv','')
+          with open(localFile, 'r',encoding='utf8') as data:
+            reader = csv.reader(data)
+            for columns in reader:
+              zip = columns[6]
+              provider = columns[0]
+              if "," in provider:
+                provider = '"' + provider + '"'
+              if zip[0:5] == zipCode and 'Evusheld' == columns[8]:
+                f.write(timeStamp + ',' + zip + ',' + provider)
+                for i in range(9, 14):
+                  if i < len(columns):
+                    f.write(',' + columns[i])
+                  else:
+                    f.write(',')
+                f.write('\n')
+        f.close()
 
-with open("../../data/therapeutics-last-processed.txt", "w") as lastProcessed_file:
-  lastProcessedDate = lastProcessed_file.write(newLastProcessDate)
+    with open("../../data/therapeutics-last-processed.txt", "w") as lastProcessed_file:
+      lastProcessedDate = lastProcessed_file.write(newLastProcessDate)
