5605746c3837a77e39392ded1cbd65710ad1a488
Rob Relyea <rob@relyeas.net>
2022-03-12 02:07:58 -0800

    script to keep zip code dose details up to date for all mAbs. Fixes #13

--- src/process-therapeutics.py
+++ src/process-therapeutics.py
@@ -8,85 +8,99 @@ import requests
 from urllib.parse import urlparse
 import csv
 
-mab = sys.argv[1] if len(sys.argv) > 1 else ""
+def get5digitZip(rawZip):
+  if len(rawZip) == 3:
+    return '00' + rawZip
+  elif len(rawZip) == 4:
+    return '0' + rawZip 
+  elif len(rawZip) == 5:
+    return rawZip 
+  elif len(rawZip) > 5:
+    return rawZip[0:5]
+    
+def updateZipCodeFilesForDrug( drugs ):
+  localBasePath = "../../"
 
-if mab == "":
-  print("need to pass in mAb name as first param (like 'Evusheld')")
-  quit()
+  with open(localBasePath + "data/therapeutics-last-processed.txt", "r") as lastProcessed_file:
+    lastProcessedDate = lastProcessed_file.readline()
+    print("last processed: " + lastProcessedDate)
 
-localBasePath = ""
+  newLastProcessedDate = None
+  with open(localBasePath + "data/therapeutics-archive.json", "r") as read_file:
+      publishings = json.load(read_file)
+      urls = []
 
-with open(localBasePath + "data/therapeutics-last-processed.txt", "r") as lastProcessed_file:
-  lastProcessedDate = lastProcessed_file.readline()
-  print("last processed: " + lastProcessedDate)
+      for publishing in publishings:
+        updateDate = publishing["update_date"]
+        if updateDate > lastProcessedDate:
+          urls.append(publishing["archive_link"]["url"])
+          newLastProcessedDate = updateDate
 
-newLastProcessDate = None
-with open(localBasePath + "data/therapeutics-archive.json", "r") as read_file:
-    publishings = json.load(read_file)
-    urls = []
-
-    for publishing in publishings:
-      updateDate = publishing["update_date"]
-      if updateDate > lastProcessedDate:
-        urls.append(publishing["archive_link"]["url"])
-        newLastProcessDate = updateDate
-
-# download all new files
-for url in sorted(urls):
-  filename = os.path.basename(urlparse(url).path)
-  targetPath = './data/therapeutics/'
-  while not os.path.exists(targetPath):
-    os.mkdir(targetPath)
-  localFile = localBasePath + 'data/therapeutics/' + filename
-  if not exists(localFile):
-    r = requests.get(url, allow_redirects=True)
-    open(localFile, 'wb').write(r.content)
-
-if len(urls) > 0:
-    # calculate all zip codes, by looking at latest data file
-    # note - if a provider was listed earlier, but isn't in the latest data file, their zip code might not be processed
-    #      - tracking this problem here: https://github.com/rrelyea/evusheld-locations-history/issues/11
-    zipSet = set()
-    url = sorted(urls)[len(urls) - 1]
+  # download all new files
+  for url in sorted(urls):
     filename = os.path.basename(urlparse(url).path)
-    localFile = localBasePath + 'data/therapeutics/'+filename
-    with open(localFile, 'r',encoding='utf8') as data:
-      reader = csv.reader(data)
-      for columns in reader:
-        zip = columns[6]
-        if zip != "Zip" and mab == columns[8]:
-          zipSet.add(zip[0:5])
+    targetPath = localBasePath + 'data/therapeutics/'
+    while not os.path.exists(targetPath):
+      os.mkdir(targetPath)
+    mabsFile = localBasePath + 'data/therapeutics/' + filename
+    if not exists(mabsFile):
+      print("downloading " + url)
+      r = requests.get(url, allow_redirects=True)
+      therapeuticsFile = open(mabsFile, 'wb')
+      therapeuticsFile.write(r.content)
+      therapeuticsFile.close()
 
-    print(str(len(zipSet)) + ' zipcodes:')
-    for zipCode in sorted(zipSet):
-      print(zipCode, end=', ', flush=True)
-
-      targetPath = './data/dose-details/' + mab.lower() + '/'
+  for drug in drugs:
+      targetPath = localBasePath + 'data/dose-details/' + drug.lower() + '/'
       while not os.path.exists(targetPath):
         os.mkdir(targetPath)
-      with open(localBasePath + 'data/dose-details/' + mab.lower() + '/' + str(zipCode)+'.csv', "a") as f:
-        for url in sorted(urls):
-          filename = os.path.basename(urlparse(url).path)
-          localFile = 'archive/'+filename
-          timeStamp = filename.replace('rxn6-qnx8_','').replace('.csv','')
-          with open(localFile, 'r',encoding='utf8') as data:
-            reader = csv.reader(data)
-            for columns in reader:
-              zip = columns[6]
-              provider = columns[0]
-              if "," in provider:
-                provider = '"' + provider + '"'
-              if zip[0:5] == zipCode and mab == columns[8]:
-                f.write(timeStamp + ',' + zip + ',' + provider)
-                for i in range(9, 14):
-                  if i < len(columns):
-                    f.write(',' + columns[i])
-                  else:
-                    f.write(',')
-                f.write('\n')
-        f.close()
 
-    # update lastProcessedDate unless passed in a flag not to.
-    if len(sys.argv) < 3 or sys.argv[2] != "updateLastProcessed=false":
-      with open("../../data/therapeutics-last-processed.txt", "w") as lastProcessed_file:
-        lastProcessedDate = lastProcessed_file.write(newLastProcessDate)
+  # calculate all zip codes, by looking at latest data file
+  url = sorted(urls)[len(urls) - 1]
+  filename = os.path.basename(urlparse(url).path)
+  targetPath = localBasePath + 'data/therapeutics/'
+  while not os.path.exists(targetPath):
+    os.mkdir(targetPath)
+  mabsFile = localBasePath + 'data/therapeutics/' + filename
+  therapeuticsFile = open(mabsFile, 'r', encoding='utf8')
+  zipSet = set()
+  reader = csv.reader(therapeuticsFile)
+  for columns in reader:
+    zip = get5digitZip(columns[6])
+    if zip != "00Zip" and (columns[8] in drugs) and zip[0] == '0':
+      zipSet.add(zip)
+  therapeuticsFile.close()
+
+  print('zip codes for ' + mabsFile + ':' + str(len(zipSet)))
+
+  for zipCode in sorted(zipSet):
+    print(zipCode, end=', ', flush=True)
+    zipFile = [None] * len(drugs)
+    filename = os.path.basename(urlparse(url).path)
+    therapeuticsFile = localBasePath + 'data/therapeutics/'+filename
+    timeStamp = filename.replace('rxn6-qnx8_','').replace('.csv','')
+    with open(therapeuticsFile, 'r', encoding='utf8') as data:
+      reader = csv.reader(data)
+      for columns in reader:
+        zip = get5digitZip(columns[6])
+        provider = columns[0]
+        if "," in provider:
+          provider = '"' + provider + '"'
+        if zip == zipCode and columns[8] in drugs:
+          index = drugs.index(columns[8])
+          if zipFile[index] == None:
+            zipFile[index] = open(localBasePath + 'data/dose-details/' + columns[8].lower() + '/' + str(zipCode)+'.csv', "a",encoding='utf8')
+          f = zipFile[index]
+          f.write(timeStamp + ',' + zip + ',' + provider)
+          for i in range(9, 14):
+            if i < len(columns):
+              f.write(',' + columns[i])
+            else:
+              f.write(',')
+          f.write('\n')
+  return newLastProcessedDate
+
+lastProcessedDate = updateZipCodeFilesForDrug(['Evusheld','Paxlovid','Sotrovimab', 'Bebtelovimab'])
+with open("../../data/therapeutics-last-processed.txt", "w") as lastProcessed_file:
+  lastProcessed_file.write(lastProcessedDate)
+
