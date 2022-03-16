8be8f3683a98e360e2df62e1b7b4ac2d7f5bcb72
Rob Relyea <rob@relyeas.net>
2022-03-16 16:27:20 -0700

    use all zip codes, not just those that start with 0. commit updates even if zip code process is broken.

--- src/process-therapeutics.py
+++ src/process-therapeutics.py
@@ -69,7 +69,7 @@ def updateZipCodeFilesForDrug(localBasePath, drugs):
   reader = csv.reader(therapeuticsFile)
   for columns in reader:
     zip = get5digitZip(columns[6])
-    if zip != "00Zip" and (columns[8] in drugs) and zip[0] == '0':
+    if zip != "00Zip" and (columns[8] in drugs):
       zipSet.add(zip)
   therapeuticsFile.close()
 
